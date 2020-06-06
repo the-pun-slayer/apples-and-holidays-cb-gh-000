ed6abdb9987d84de60576355b70184dea4caf4a9
PUNEET MANDIL <puneetmandil.mec18@itbhu.ac.in>
2020-06-06 19:16:06 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,6 +18,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  
   holiday_hash[:summer][:fourth_of_july][1]
   
 end
@@ -28,7 +29,9 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
   
   holiday_hash[:winter][:Christmas]<<supply
+  
   holiday_hash[:winter][:new_years]<<supply
+  
   holiday_hash
 
 end
